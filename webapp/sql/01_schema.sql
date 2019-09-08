a26556bf72e55fed9246189e7d356f42914107c5
zaq1tomo <zaq1tomo@gmail.com>
2019-09-08 12:41:38 +0900

    Add idx_seller_id and idx_buyer_id

--- webapp/sql/01_schema.sql
+++ webapp/sql/01_schema.sql
@@ -31,6 +31,8 @@ CREATE TABLE `items` (
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   INDEX idx_category_id (`category_id`),
+  INDEX idx_seller_id(`seller_id`),
+  INDEX idx_buyer_id(`buyer_id`),
   INDEX idx_created_at(`created_at`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET utf8mb4;
 
