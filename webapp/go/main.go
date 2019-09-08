a5ef93aebb2d16e4cf07c8dfc7fcf1b3a20c077f
zaq1tomo <zaq1tomo@gmail.com>
2019-09-08 17:58:33 +0900

    Remove pprof

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -10,7 +10,6 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-	_ "net/http/pprof"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -284,11 +283,6 @@ func init() {
 }
 
 func main() {
-	// TODO: Remove
-	go func() {
-		log.Println(http.ListenAndServe("localhost:6060", nil))
-	}()
-
 	host := os.Getenv("MYSQL_HOST")
 	if host == "" {
 		host = "127.0.0.1"
