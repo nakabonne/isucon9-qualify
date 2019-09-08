79eeca92398b1207323c6958f17cedfc5ce5c1c0
zaq1tomo <zaq1tomo@gmail.com>
2019-09-08 17:07:46 +0900

    fix

--- webapp/sql/01_schema.sql
+++ webapp/sql/01_schema.sql
@@ -49,7 +49,7 @@ CREATE TABLE `transaction_evidences` (
   `item_category_id` int unsigned NOT NULL,
   `item_root_category_id` int unsigned NOT NULL,
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   INDEX idx_item_id(`item_id`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET utf8mb4;
 
